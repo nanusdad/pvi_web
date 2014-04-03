8ff84cd6ae9f976e4db97cf1fc1fa0c13028ccf0
Arun Zechariah <arun@zechariah.net>
2014-04-03 00:56:40 -0500

    whole lot of updates

--- client/lib/tdata.js
+++ client/lib/tdata.js
@@ -1,6 +1,7 @@
 slides = [ {
 	image_url: 'fishing.JPG',
 	caption: "proVISION India",
+  tagline: "proVISION INDIA’s core competence lies in helping people as well as organizations develop practical, sustainable ways of income-generation that benefit both the individuals and their communities, enabling them to reach their full potential.",
 	sstate: "active"
 },{
 	image_url: 'women_training.JPG',
@@ -27,7 +28,43 @@ rroutes = [{
   rpath: '/contact',
   rtemplate: 'contact'
 },{
+  rname: 'training',
+  rpath: '/training',
+  rtemplate: 'training'
+},{
+  rname: 'get_involved',
+  rpath: '/get_involved',
+  rtemplate: 'get_involved'
+},{
+  rname: 'gallery',
+  rpath: '/gallery',
+  rtemplate: 'gallery'
+},{
+  rname: 'sidebar',
+  rpath: '/sidebar',
+  rtemplate: 'sidebar'
+},{
+  rname: 'training_center',
+  rpath: '/training_center',
+  rtemplate: 'training_center'
+},{
+  rname: 'pricing',
+  rpath: '/pricing',
+  rtemplate: 'pricing'
+},{
   rname: 'portfolio1',
   rpath: '/portfolio1',
   rtemplate: 'portfolio1'
+},{
+  rname: 'portfolio3',
+  rpath: '/portfolio3',
+  rtemplate: 'portfolio3'
+},{
+  rname: 'portfolio4',
+  rpath: '/portfolio4',
+  rtemplate: 'portfolio4'
+},{
+  rname: 'camps',
+  rpath: '/camps',
+  rtemplate: 'camps'
 }];
